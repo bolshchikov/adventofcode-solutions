64c89fc67bd98a8a420ab1f2cf86c576c33eaaf7
Sergey Bolshchikov <sergey@bolshchikov.net>
2017-12-02 23:23:18 +0200

    chore(day_1): pass input by reference

--- 2017/day_1/src/main.rs
+++ 2017/day_1/src/main.rs
@@ -3,7 +3,7 @@ use std::io::prelude::*;
 
 const INPUT_FILE_NAME: &str = "input.txt";
 
-fn str_to_int(input: String) -> Vec<u32> {
+fn str_to_int(input: &str) -> Vec<u32> {
     let mut numbers: Vec<u32> = Vec::new();
 
     let input = input.trim();
@@ -17,13 +17,12 @@ fn str_to_int(input: String) -> Vec<u32> {
 }
 
 fn main() {
-
     let mut f = File::open(INPUT_FILE_NAME).expect("file not found");
     let mut input = String::new();
 
     f.read_to_string(&mut input).expect("Failed to read input");
 
-    let numbers = str_to_int(input);
+    let numbers = str_to_int(&input);
     let mut res: Vec<u32> = Vec::new();
     let mut prev = &numbers[numbers.len() - 1];
 
