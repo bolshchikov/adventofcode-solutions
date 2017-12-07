550339ed4862b8ba5a43dca12279a4df6fd00cd5
Sergey Bolshchikov <sergey@bolshchikov.net>
2017-12-07 21:06:51 +0200

    chore(2017): day_2 implementation

--- 2017/day_2/src/main.rs
+++ 2017/day_2/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+const INPUT_FILE_NAME: &str = "input.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let mut difference: Vec<u32> = Vec::new();
+    let f = File::open(INPUT_FILE_NAME).expect("file not found");
+    let file = BufReader::new(&f);
+    for line in file.lines() {
+        let l = line.unwrap();
+
+        let max: u32 = l.split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .max()
+            .expect("could not get max value");
+        let min: u32 = l.split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .min()
+            .expect("could not get min value");
+
+        difference.push(max - min);
+    }
+
+    let res: u32 = difference.iter().sum();
+    println!("{}", res);
 }
